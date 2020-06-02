de477c0bc837574b18698e423afa9035f620e99c
vortex314 <lieven.merckx@gmail.com>
2020-06-02 20:23:48 +0200

    shaker

--- src/main/scala/be/limero/brain/TreeShaker.scala
+++ src/main/scala/be/limero/brain/TreeShaker.scala
@@ -1,6 +1,7 @@
 package be.limero.brain
 
 import java.net.InetAddress
+import java.util.Calendar
 
 import org.slf4j.{Logger, LoggerFactory}
 
@@ -39,15 +40,17 @@ class Poller(thread: NanoThread) extends Actor(thread) {
     })
   }
 }
+
 // rescale to another frame
 case class Scale(x1: Int, x2: Int, y1: Int, y2: Int) extends Flow[Int, Int] {
   def on(i: Int): Unit = {
     var out = y1 + (i * (y2 - y1)) / (x2 - x1)
-    if ( out < y1 ) out =y1;
-    if ( out > y2 ) out=y2
+    if (out < y1) out = y1;
+    if (out > y2) out = y2
     emit(out)
   }
 }
+
 // round to the next multiple of step
 case class Step(step: Int) extends Flow[Int, Int] {
   def on(value: Int): Unit = {
@@ -56,6 +59,7 @@ case class Step(step: Int) extends Flow[Int, Int] {
     emit(v)
   }
 }
+
 // if abs(value) < zero send 0
 case class Zero(zero: Int) extends Flow[Int, Int] {
   def on(value: Int): Unit = {
@@ -66,19 +70,21 @@ case class Zero(zero: Int) extends Flow[Int, Int] {
 
 // if abs(value) < zero send 0
 case class Toggle() extends Flow[Boolean, Boolean] {
-  var lastValue=false
+  var lastValue = false
+
   def on(value: Boolean): Unit = {
     if (value) {
-      lastValue= !lastValue
+      lastValue = !lastValue
       emit(lastValue)
     }
     else emit(value)
   }
 }
+
 // log the item
 case class Log[T](msg: String) extends Flow[T, T] {
   def on(t: T): Unit = {
-    log.info(msg + ":"+ t)
+    log.info(msg + ":" + t)
     emit(t)
   }
 }
@@ -95,84 +101,112 @@ case class Changed[T](timeout: Int, var oldValue: T) extends Flow[T, T] {
   }
 }
 
-case class Rtc(fromTime:Time,toTime) extends ValueSource[Boolean] {
 
-}
-
-class TreeShaker(thread:NanoThread) extends Actor(thread) {
-  val dayTime : ValueFlow[Boolean]
-  val intervalTimer = TimerSource(thread,1,5*60000,true);
-  def init = {
-    dayTime >> (bool => if (bool) intervalTimer.stop() else intervalTimer.start() )
-    intervalTimer >> mqtt.to[Boolean]("dst/treeshaker/shaker/shake")
-  }
-}
-
-object TreeShaker {
-  val log: Logger = LoggerFactory.getLogger(classOf[Thread])
-
-  def main(args: Array[String]): Unit = {
-
-    val mainThread = NanoThread("main")
-    val mqttThread = NanoThread("mqtt")
-    val mqtt = new Mqtt(mqttThread)
-    val timer = TimerSource(mainThread, 1, 10000, repeat = true)
-    val upTime = new LambdaSource[Long](() => Sys.millis)
-    val ipAddress = new LambdaSource[String](() => InetAddress.getLocalHost.toString)
-    val poller = new Poller(mainThread)
-    val echo = new Echo(mainThread)
-    val sender = new Sender(mainThread, 1000000)
-
-    if ( Sys.hostname.compareToIgnoreCase("pi3")==0)  mqtt.brokerUrl="tcp://localhost:1883"
-    else mqtt.brokerUrl="tcp://limero.ddns.net:1883"
-    mqtt.init()
-    poller.init()
-    val vs = new ValueSource[Int](1)
-    vs >> mqtt.to[Int]("system/counter")
-    upTime >> mqtt.to[Long]("system/upTime")
-    ipAddress >> mqtt.to[String]("wifi/ipAddress")
-    mqtt.from[Int]("system/counter") >> (i => {
-      log.info("i=" + i)
-    })
-    timer >> (_ => {
-      vs() = vs() + 1
-      log.info("Cas fails " + NanoAkka.bufferCasRetries+
-        " overflow: "+NanoAkka.bufferOverflow+
-        " busyPop : "+NanoAkka.bufferPopBusy+
-        " busyPush "+NanoAkka.bufferPushBusy)
-    })
- //   timer >> new LambdaFlow[TimerMsg,Int]( _ =>  vs() )  >> mqtt.to[Int]("src/remote/remote/potLeft")
-    mqtt.from[Int]("src/remote/remote/potLeft") >>
-      Scale(0, 1023, -90, +90) >>
-      Step(1) >>
-      Zero(5) >>
- //     Changed(1000, 0) >>
-      Log[Int]("angleTarget:") >>
-      mqtt.to[Int]("dst/drive/stepper/angleTarget")
-
-    mqtt.from[Int]("src/remote/remote/potRight") >>
-      Scale(0, 1023, -200, +200) >>
-      Step(5) >>
-      Zero(20) >>
- //     Changed(1000, 0) >>
-      Log[Int]("rpmTarget:") >>
-      mqtt.to[Int]("dst/drive/motor/rpmTarget")
-
-    mqtt.from[Boolean]("src/remote/remote/buttonLeft") >>
- //     Changed[Boolean](1000, false) >>
-//      Log[Boolean]("buttonLeft:") >>
-      mqtt.to[Boolean]("dst/cutter/cutter/on")
-
-    mqtt.from[Boolean]("src/remote/system/alive") >> Toggle() >>  mqtt.to[Boolean]("dst/remote/remote/ledLeft")
-    mqtt.from[Boolean]("src/drive/system/alive") >> Toggle() >>  mqtt.to[Boolean]("dst/remote/remote/ledRight")
-    mqtt.from[Boolean]("src/remote/system/alive") >> mqtt.to[Boolean]("dst/drive/motor/watchdogReset");
-    mqtt.from[Boolean]("src/remote/system/alive") >> mqtt.to[Boolean]("dst/drive/stepper/watchdogReset");
-    poller(upTime, ipAddress)
-    echo.out >> sender.in
-    sender.out >> echo.in
-    //    echo.in.on(1)
-    mqttThread.start()
-    mainThread.run()
+case class TimeOfDay(hour: Int, min: Int) {
+  def compareNow: Int = {
+    val now = System.currentTimeMillis()
+    val nw = Calendar.getInstance()
+    val hr = nw.get(Calendar.HOUR_OF_DAY)
+    val mn = nw.get(Calendar.MINUTE)
+    if (hr > hour) {
+      1
+    } else if (hr == hour) {
+      if (mn == min) {
+        0
+      }
+    }
   }
 
-}
+  class DayLight(thread: NanoThread, from: TimeOfDay, to: TimeOfDay) extends Actor(thread) {
+    val ts = TimerSource(thread, 1, 5 * 60000, true)
+    val isDaylight: ValueFlow[Boolean]
+
+    def nowDayLight: Boolean = {
+
+    }
+
+    def init = {
+      ts >> (tm: TimerMsg => {
+
+      })
+    }
+  }
+
+  class TreeShaker(thread: NanoThread) extends Actor(thread) {
+    val dayTime: ValueFlow[Boolean]
+    val intervalTimer = TimerSource(thread, 1, 5 * 60000, true);
+
+    def init = {
+      dayTime >> (bool => if (bool) intervalTimer.stop() else intervalTimer.start())
+      intervalTimer >> mqtt.to[Boolean]("dst/treeshaker/shaker/shake")
+    }
+  }
+
+  object TreeShaker {
+    val log: Logger = LoggerFactory.getLogger(classOf[Thread])
+
+    def main(args: Array[String]): Unit = {
+
+      val mainThread = NanoThread("main")
+      val mqttThread = NanoThread("mqtt")
+      val mqtt = new Mqtt(mqttThread)
+      val timer = TimerSource(mainThread, 1, 10000, repeat = true)
+      val upTime = new LambdaSource[Long](() => Sys.millis)
+      val ipAddress = new LambdaSource[String](() => InetAddress.getLocalHost.toString)
+      val poller = new Poller(mainThread)
+      val echo = new Echo(mainThread)
+      val sender = new Sender(mainThread, 1000000)
+
+      if (Sys.hostname.compareToIgnoreCase("pi3") == 0) mqtt.brokerUrl = "tcp://localhost:1883"
+      else mqtt.brokerUrl = "tcp://limero.ddns.net:1883"
+      mqtt.init()
+      poller.init()
+      val vs = new ValueSource[Int](1)
+      vs >> mqtt.to[Int]("system/counter")
+      upTime >> mqtt.to[Long]("system/upTime")
+      ipAddress >> mqtt.to[String]("wifi/ipAddress")
+      mqtt.from[Int]("system/counter") >> (i => {
+        log.info("i=" + i)
+      })
+      timer >> (_ => {
+        vs() = vs() + 1
+        log.info("Cas fails " + NanoAkka.bufferCasRetries +
+          " overflow: " + NanoAkka.bufferOverflow +
+          " busyPop : " + NanoAkka.bufferPopBusy +
+          " busyPush " + NanoAkka.bufferPushBusy)
+      })
+      //   timer >> new LambdaFlow[TimerMsg,Int]( _ =>  vs() )  >> mqtt.to[Int]("src/remote/remote/potLeft")
+      mqtt.from[Int]("src/remote/remote/potLeft") >>
+        Scale(0, 1023, -90, +90) >>
+        Step(1) >>
+        Zero(5) >>
+        //     Changed(1000, 0) >>
+        Log[Int]("angleTarget:") >>
+        mqtt.to[Int]("dst/drive/stepper/angleTarget")
+
+      mqtt.from[Int]("src/remote/remote/potRight") >>
+        Scale(0, 1023, -200, +200) >>
+        Step(5) >>
+        Zero(20) >>
+        //     Changed(1000, 0) >>
+        Log[Int]("rpmTarget:") >>
+        mqtt.to[Int]("dst/drive/motor/rpmTarget")
+
+      mqtt.from[Boolean]("src/remote/remote/buttonLeft") >>
+        //     Changed[Boolean](1000, false) >>
+        //      Log[Boolean]("buttonLeft:") >>
+        mqtt.to[Boolean]("dst/cutter/cutter/on")
+
+      mqtt.from[Boolean]("src/remote/system/alive") >> Toggle() >> mqtt.to[Boolean]("dst/remote/remote/ledLeft")
+      mqtt.from[Boolean]("src/drive/system/alive") >> Toggle() >> mqtt.to[Boolean]("dst/remote/remote/ledRight")
+      mqtt.from[Boolean]("src/remote/system/alive") >> mqtt.to[Boolean]("dst/drive/motor/watchdogReset");
+      mqtt.from[Boolean]("src/remote/system/alive") >> mqtt.to[Boolean]("dst/drive/stepper/watchdogReset");
+      poller(upTime, ipAddress)
+      echo.out >> sender.in
+      sender.out >> echo.in
+      //    echo.in.on(1)
+      mqttThread.start()
+      mainThread.run()
+    }
+
+  }
